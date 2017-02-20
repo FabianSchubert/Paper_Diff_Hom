c1ff58042d55a188ad4aca41123cd3f512917772
Fabian Schubert <fj.schubert@freenet.de>
2017-02-21 00:41:57 +0100

    .

--- plots_for_paper/plot_frequ_dist_mult_sets.py
+++ plots_for_paper/plot_frequ_dist_mult_sets.py
@@ -1,20 +1,24 @@
 import numpy as np
 import cPickle as pickle
 import matplotlib.pyplot as plt
-from frequ_from_spikes import *
-from plot_setting import *
+from custom_modules.frequ_from_spikes import *
+from custom_modules.plot_setting import *
 import sys
+import json
 
 files_diff = []
 files_non_diff = []
-files_instant_diff = []
 
-for k in xrange(10):
-	files_diff.append(sim_data_base_folder + "spiketime_dataset_diff/set_"+str(k)+"/")
-	files_non_diff.append(sim_data_base_folder + "spiketime_dataset_non_diff/set_"+str(k)+"/")
+folder_diff = sim_data_base_folder + "spiketime_dataset_diff/"
+folder_non_diff = sim_data_base_folder + "spiketime_dataset_non_diff/"
+folder_instant_diff = sim_data_base_folder + "spiketime_dataset_instant_diffusion/"
+
+#for k in xrange(10):
+#	files_diff.append(sim_data_base_folder + "spiketime_dataset_diff/set_"+str(k)+"/")
+#	files_non_diff.append(sim_data_base_folder + "spiketime_dataset_non_diff/set_"+str(k)+"/")
 
 
-savefold = plots_base_folder + "firing_rate_dists/"
+savefold = plots_base_folder + "paper/"
 
 n_bins = 40
 
@@ -29,7 +33,7 @@ if plot_pop == "exc":
 	title_reg = "A"
 	title_log = "B"
 	plot_filename = "fir_rate_dist_e_compare"
-	filename = "spiketimes_e.p"
+	
 elif plot_pop == "inh":
 	#### inhibitory
 	regular_bin_range = [0.,25.]
@@ -39,7 +43,7 @@ elif plot_pop == "inh":
 	title_reg = "A*"
 	title_log = "B*"
 	plot_filename = "fir_rate_dist_i_compare"
-	filename = "spiketimes_i.p"
+	
 else:
 	print "Wrong Population Argument"
 	sys.exit()
@@ -47,16 +51,26 @@ else:
 
 
 
-def plot_dist(files,filename,t_range,bins_hist,plot_type,label_hist,ax):
+def plot_dist(folder,population,t_range,bins_hist,plot_type,label_hist,ax):
 
 	spt_sets = []
 	spt_sets_joint = []
 
-
-	for k in xrange(len(files)):
+	with open(folder + "data_list.dat","rb") as datafile:
+		for row in datafile:
+			data = json.loads(row)
+			if population == "exc":
+				spt_sets.append(data["spt_e"])
+			elif population == "inh":
+				spt_sets.append(data["spt_i"])
+			else:
+				sys.exit()
+			spt_sets_joint.extend(spt_sets[-1])
 	
-		spt_sets.append(pickle.load(open(files[k]+filename)).values())
-		spt_sets_joint.extend(spt_sets[-1])
+#	for k in xrange(len(files)):
+#	
+#		spt_sets.append(pickle.load(open(files[k]+filename)).values())
+#		spt_sets_joint.extend(spt_sets[-1])
 
 
 	n_total = len(spt_sets_joint)
@@ -71,13 +85,15 @@ def plot_dist(files,filename,t_range,bins_hist,plot_type,label_hist,ax):
 	
 	ax.hist(f_arr,bins=bins_hist,normed=True,histtype="step",label=label_hist)
 
-fig, ax = plt.subplots(1,2,figsize=(default_fig_width,default_fig_width*0.4))
+fig, ax = plt.subplots(1,2,figsize=(default_fig_width,default_fig_width*0.5))
 
 bins_h_regular = np.linspace(regular_bin_range[0],regular_bin_range[1],n_bins+1)
 bins_h_log = np.linspace(log_bin_range[0],log_bin_range[1],n_bins+1)
 
-plot_dist(files_non_diff,filename,[1000.,1500.],bins_h_regular,"regular","Non-Diff. H.",ax[0])
-plot_dist(files_diff,filename,[1000.,1500.],bins_h_regular,"regular","Diff. H.",ax[0])
+plot_dist(folder_non_diff,plot_pop,[1000.,1500.],bins_h_regular,"regular","Non-Diff. H.",ax[0])
+plot_dist(folder_diff,plot_pop,[1000.,1500.],bins_h_regular,"regular","Diff. H.",ax[0])
+plot_dist(folder_instant_diff,plot_pop,[1000.,1500.],bins_h_regular,"regular","Instant Diff. H.",ax[0])
+
 if plot_pop == "exc":
 	ax[0].legend()
 ax[0].set_title(title_reg,loc="left")
@@ -86,8 +102,9 @@ ax[0].set_xlim(regular_bin_range[0],regular_bin_range[1])
 ax[0].set_xlabel("f [Hz]")
 ax[0].set_ylabel("Prob. Dens.")
 
-plot_dist(files_non_diff,filename,[1000.,1500.],bins_h_log,"log","Non-Diff. H.",ax[1])
-plot_dist(files_diff,filename,[1000.,1500.],bins_h_log,"log","Diff. H.",ax[1])
+plot_dist(folder_non_diff,plot_pop,[1000.,1500.],bins_h_log,"log","Non-Diff. H.",ax[1])
+plot_dist(folder_diff,plot_pop,[1000.,1500.],bins_h_log,"log","Diff. H.",ax[1])
+plot_dist(folder_instant_diff,plot_pop,[1000.,1500.],bins_h_log,"log","Instant Diff. H.",ax[1])
 #ax[1].legend()
 ax[1].set_title(title_log,loc="left")
 ax[1].set_ylim(y_lim_log)
@@ -105,8 +122,7 @@ plt.show()
 
 
 
-#import pdb
-#pdb.set_trace()
+
 
 
 
